970dbe25923d97a06b9999148eeac91586a96d10
JM Labesse <jeanmarie.labesse@gmail.com>
2017-05-19 09:51:38 +0200

    Résolution algo

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -12,7 +12,18 @@ class AlgoController extends Controller
     //
 
     public function dateInterval ($series) {
+        ksort($series);
+        $years = array_keys($series);
+        $max_diff = 0;
 
+        for($ind = 0; $ind < sizeof($years) -1; $ind++) {
+            $currentDiff = $years[$ind+1]-$years[$ind];
+            if ($currentDiff > $max_diff) {
+                $max_diff = $currentDiff;
+            }
+        }
+
+        return $max_diff;
     }
 
 }
