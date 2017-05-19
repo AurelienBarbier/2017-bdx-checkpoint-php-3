ac7c4f3023bac0fb2de2d605127cdd7277bb1b53
RebeccaTim <rebecca.timahieu@outlook.fr>
2017-05-19 11:25:13 +0200

    resolution algo

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -13,6 +13,23 @@ class AlgoController extends Controller
 
     public function dateInterval ($series) {
 
+    	$a_keys = array_keys($series);
+    	sort($a_keys, SORT_NUMERIC);
+    	print_r($a_keys);
+
+    	$a_diff = array();
+
+    	for ($i = 0; $i < count($a_keys)-1; $i++){
+    		  			
+    		if($a_keys[$i +1]){ 
+    			$diff = abs(abs($a_keys[$i])-abs($a_keys[$i +1]));
+    		
+    		array_push($a_diff, $diff);
+    		
+    		}
+    	}
+    	return (max($a_diff));
+
     }
 
 }
