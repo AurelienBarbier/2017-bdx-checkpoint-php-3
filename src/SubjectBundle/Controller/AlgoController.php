9bdf1d9cc5c20661ceb5a8d3d5f224a00a21920c
Alex-san <bobbax4@live.fr>
2017-05-19 09:51:19 +0200

    Algo terminer et fonctionnel.

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -12,7 +12,19 @@ class AlgoController extends Controller
     //
 
     public function dateInterval ($series) {
-
+        uksort($series, function($a, $b){
+            if ($a == $b) {
+                return 0;
+            }
+            return ($a < $b) ? -1 : 1;
+        });
+        $series = array_values(array_flip($series));
+        $maxdifference = 0;
+        for ($i = 0; $i < count($series)-1; $i++){
+            $difference =  $series[$i+1] - $series[$i];
+            $maxdifference = $difference > $maxdifference ? $difference : $maxdifference;
+        }
+        return $maxdifference;
     }
 
 }
