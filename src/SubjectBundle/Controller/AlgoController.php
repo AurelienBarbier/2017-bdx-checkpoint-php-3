9709b5cba4b430e5b001e59f2db7a56414b07be2
Lélé <aurelien@wildcodeschool.fr>
2017-05-24 11:11:14 +0200

    Resultat Dojo

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -12,6 +12,31 @@ class AlgoController extends Controller
     //
 
     public function dateInterval ($series) {
+        ksort($series);
+        $year = array_keys($series);
+        $diffTab = [];
+        for($i=0; $i<count($year)-1; $i++){
+            $ecart = $year[$i+1]-$year[$i];
+            array_push($diffTab, $ecart);
+        }
+        return max($diffTab);
+
+        /*$precedent = 0;
+        $ecart = 0;
+        $diff=0;
+        foreach($series as $year=>$show) {
+            if($precedent > 0) {
+                $diff=$year - $precedent;
+                if ($diff > $ecart) {
+                    $ecart = $diff;
+                }
+            }
+            $precedent = $year;
+        }
+
+
+
+            return $ecart;*/
 
     }
 
